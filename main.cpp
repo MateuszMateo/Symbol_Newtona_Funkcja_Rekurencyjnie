219a79dc7da92833a9b1feba14f8e0f850cc32b9
MateuszMateo <k.mateusz120@gmail.com>
2015-10-11 18:36:09 +0200

    Wprowadzenie szkicu kodu

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+long long Symbol(long long n,long long k){
+    if(k==0,k==n){
+        return 1;
+        }
+    else{
+        return Symbol(n,k)=Symbol(n-1, k-1) + Symbol(n-1,k);
+    }
+
+}
+
+int main()
+{
+    cout << "Podaj wartosci" << endl;
+    int n,k;
+    cin >>k>>n;
+    cout << Symbol(n,k)<<endl;
+    return 0;
+}
+
